e965a1fedaf61d325e378562e4e769ffbdee0313
aprilart <aprilr2g2@gmail.com>
2023-04-17 08:57:26 -0700

    First commit.

--- app.js
+++ app.js
@@ -2,31 +2,32 @@ const quoteText = document.getElementById("quote");
 const quoteBtn = document.getElementById("quoteBtn");
 const authorText = document.getElementById("author");
 
-quoteBtn.addEventListener("click", generateQuotes)
+quoteBtn.addEventListener("click", generateQuotes);
 
-generateQuotes()
+generateQuotes();
 
 async function generateQuotes() {
-    const res = await fetch("https://zenquotes.io/api/random"
-    )
-    const data = await res.json();
+    const resp = await fetch("https://zenquotes.io/api/random");
+    const data = await resp.json();
     console.log(data);
     
-    let quote = ""
-       if (data[0].q == undefined) {
-        quote = "Error: no quote here"
+    let quote = "";
+    let author = "";
+       
+    if (data[0].q == undefined) {
+        quote = "Error: no quote here";
        } else {
-        quote = data[0].q
+        quote = data[0].q;
        }
-    quoteText.innerHTML = ` "${quote}" `
 
-    let author = ""
+    quoteText.innerHTML = ` "${quote}" `;
+
        if (data[0].a == undefined) {
-        author = "Author unknown"
+        author = "Author Unknown";
        } else {
-        author = data[0].a
+        author = data[0].a;
        }
-    authorText.innerHTML = ` - ${author}`
 
+    authorText.innerHTML = ` - ${author}`;
 
 }
